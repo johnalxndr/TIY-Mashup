a70c49ff4c0ae8280dd1d6d86e0212b8f8a09e46
John Alexander <jalexander788@gmail.com>
2015-02-16 23:18:25 -0500

    trying to get geolocation function to give coords, getting one giant number :(

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,9 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+
+var userLatitude, userLongitude; 
+
+navigator.geolocation.watchPosition(function (position) {
+userLatitude = position.coords.latitude;
+userLongitude = position.coords.longitude;
+console.log(position);
+    });
