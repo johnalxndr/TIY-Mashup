783942f9efc0cd5a8bbbfd6ecae496249fd9ace5
Josuel C Garcia <garciajosuel@yahoo.com>
2015-02-16 22:38:37 -0500

    have a map but not where i want it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,16 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(28.541220, -81.381259),
+    zoom:15,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  icon:'http://tomboywest.co.uk/wp-content/uploads/2014/09/food-btn.png'
+  });
+
+marker.setMap(map);
