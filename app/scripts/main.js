dc7f455c3994dd4b8251e543b845ca4b93439f37
John Alexander <jalexander788@gmail.com>
2015-02-17 18:28:35 -0500

    able to populate button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,20 +2,35 @@
 //
 
 $(document).ready(function(){
+    
+    var data = {"prices":[{"localized_display_name":"uberX","duration":422,"low_estimate":4,"display_name":"uberX","product_id":"57e6c58d-8ba2-4628-b1c9-6187514731cb","distance":2.97,"surge_multiplier":1.0,"estimate":"$4-6","high_estimate":6,"currency_code":"USD"},{"localized_display_name":"uberXL","duration":422,"low_estimate":9,"display_name":"uberXL","product_id":"8a268564-02e8-4fac-9aaf-df071e120d49","distance":2.97,"surge_multiplier":1.0,"estimate":"$9-12","high_estimate":12,"currency_code":"USD"}]}    
+    
+    
+    
+//   
 //    $.getJSON('https://api.uber.com/v1/estimates/price?start_latitude=28.541220&start_longitude=-81.381259&end_latitude=28.553572&end_longitude=-81.34691&server_token=XQysa0CmCYawqY6t8v7eWsaS7Xb3rOIRb38zqulZ', function(data) {
-//        
-//    console.log(data);
-//    
-//
-//    
-//    });
+       
+//    console.log(data.prices[0].estimate);
+
+
+var price = data.prices[0].estimate;
+var estimate = Math.floor(data.prices[0].duration / 60);
     
-    var data = {"prices":[{"localized_display_name":"uberX","duration":422,"low_estimate":4,"display_name":"uberX","product_id":"57e6c58d-8ba2-4628-b1c9-6187514731cb","distance":2.97,"surge_multiplier":1.0,"estimate":"$4-6","high_estimate":6,"currency_code":"USD"},{"localized_display_name":"uberXL","duration":422,"low_estimate":9,"display_name":"uberXL","product_id":"8a268564-02e8-4fac-9aaf-df071e120d49","distance":2.97,"surge_multiplier":1.0,"estimate":"$9-12","high_estimate":12,"currency_code":"USD"}]}
+$("#time").html("Burrito Ride in: " + estimate);
+$("#time").html(+)
+
     
-    
-    console.log(data.prices[0].estimate);
-    console.log(Math.floor(data.prices[0].duration / 60));
 });
+//});
+
+
+
+
+
+//    console.log(data.prices[0].estimate);
+//    console.log(Math.floor(data.prices[0].duration / 60));
+//});
+
 
 
 
@@ -26,46 +41,6 @@ $(document).ready(function(){
 
 
 
-
-
-//
-//
-//var Estimate  = events.filter(function(item){
-//    return item.estimate == 'estimate';
-//}
-//
-//
-//$.getJSON("https://api.uber.com/v1/estimates/price?start_latitude=28.541220&start_longitude=-81.381259&end_latitude=28.553572&end_longitude=-81.34691&server_token=XQysa0CmCYawqY6t8v7eWsaS7Xb3rOIRb38zqulZ", function(key, value){
-//    $("#time").append("<p>"+value.estimate+"</p>");
-//});
-//
-//
-
-
-
-
-
-
-
-
-
-
-//function getEstimatesForUserLocation(latitude,longitude) {
-//  $.ajax({
-//    url: "https://api.uber.com/v1/estimates/price",
-//    headers: {
-//        Authorization: "Token " + uberServerToken
-//    },
-//    data: {
-//      start_latitude: userLatitude,
-//      start_longitude: userLongitude,
-//      end_latitude: chipotleLatitude,
-//      end_longitude: chipotleLongitude,
-//    },
-//    success: function(result) {
-//      console.log(result);
-//    }
-//  });
 
 
 //    var uberClientId = "WcDY4toB0xogrSdkoXj4azFMmOdNf0HA", 
