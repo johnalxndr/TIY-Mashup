b45bb5132d994e199cb4114e1c83fb0b2738588e
John Alexander <jalexander788@gmail.com>
2015-02-17 00:06:47 -0500

    added token variables as well as a test uber.json file to practice populating data as I am having a hard time accessing the real time json.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,19 @@
 /* jshint devel:true */
 
-var userLatitude, userLongitude; 
+var uberClientId = "WcDY4toB0xogrSdkoXj4azFMmOdNf0HA", 
+    uberServerToken = "XQysa0CmCYawqY6t8v7eWsaS7Xb3rOIRb38zqulZ",
+    //Uber API stuff
 
-navigator.geolocation.watchPosition(function (position) {
-console.log(position);
-userLatitude = position.coords.latitude;
-userLongitude = position.coords.longitude;
+var userLatitude, 
+    userLongitude,
+    //becoming start_latitude start_longitude
+    
+    chipotleLatitude = 28.553572, 
+    chipotleLongitude = -81.34691;
+    //becoming end_latitude end_longitude
 
+navigator.geolocation.getCurrentPosition(function (position) {
+//    console.log(position);
+    userLatitude = position.coords.latitude;
+    userLongitude = position.coords.longitude;
 });
